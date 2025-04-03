7197789a0344b5f4e6a2b3337869e92029baacf3
Pranav1326 <singhpranav130@gmail.com>
2025-04-03 17:10:51 +0530

    Chapter 2 done

---
+++ first.py
@@ -0,0 +1,29 @@
+# a = 5
+
+# b = 9
+
+# print(a+b)
+
+# name = "pranav"
+
+# print(name)
+
+# isTrue = False
+# print(isTrue)
+
+# a = True
+# b = False
+
+# c = not a
+# print(c)
+
+a = (int)(input("Enter num 1: "))
+
+b = (int)(input("Enter num 2: "))
+
+# print(type(a))
+
+c = a + b
+
+print(c)
+
